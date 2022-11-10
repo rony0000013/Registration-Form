c8a2e66f8f111343294b4ff323371c1021f1e947
vikas <raiv2004322@gmail.com>
2022-11-10 14:47:52 +0530

    Initialized the App and added Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-center py-6">
+      <h2 className="text-3xl text-gray-300 font-inter font-bold ">Student Registration Form </h2>
+    </header>
+  );
+};
+
+export default Header;
